db34199b8983bf8894673f67c723c81e14c6df4b
Navneesh Yadav <nyadav@systango.com>
2023-02-07 14:33:51 +0530

    add JS of home2 in app.js

--- JS/app.js
+++ JS/app.js
@@ -10,7 +10,6 @@ if (Submit != null) {
     user.Email = document.getElementById("Email").value;
     user.Phone_No = document.getElementById("Phone_No").value;
     user.Password = document.getElementById("Password").value;
-    user.Image=document.getElementById("upload-img").value;
     let atpos = user.Email.indexOf("@");
     let dotpos = user.Email.lastIndexOf(".");
     if (user.Name == null || user.Name == "") {
@@ -45,37 +44,50 @@ if (Submit != null) {
 // LOGIN JS
 const Submit1 = document.getElementById("Submit1");
 
-Submit1.addEventListener("click", (event) => {
-  event.preventDefault();
-  const container = document.getElementById("container-1");
-  let email = document.getElementById("email").value;
-  let password = document.getElementById("password").value;
-  let users = JSON.parse(localStorage.getItem("users"));
-  let atpos = email.indexOf("@");
-  let dotpos = email.lastIndexOf(".");
-  if (email === null || email === "" || atpos < 1 || dotpos - atpos < 2) {
-    alert("Please provide your correct email ID!");
-    document.getElementById("email").focus();
-    return;
-  }
-  for (let i = 0; i < users.length; i++) {
-    console.log(users);
-    if (users[i].Email === email && users[i].Password === password) {
-
-
-  //     container.innerHTML = `<a href="home.html" id="back-button"><i class="fa-regular fa-circle-left fa-2x"></i></a><h1 class="heading-company">Systango</h1><div class="profile-container"> <h1 id="profile-heading">Profile Information <i class="fa-solid fa-user"></i></h1> <p class="profile"><b>Name:</b> ${users[i].Name}</p><p class="profile"><b>Email:</b> ${users[i].Email}</p><p class="profile"><b>Phone No.:</b> ${users[i].Phone_No}</p>
-  //       <a href="/login.html"
-  //   ><button>LogOut</button></a
-  // ></div>`;
-  localStorage.setItem("users1",JSON.stringify(users[i]));
- 
-  window.location.href="home2.html";
+if(Submit1 != null){
+  Submit1.addEventListener("click", (event) => {
+    event.preventDefault();
+    const container = document.getElementById("container-1");
+    let email = document.getElementById("email").value;
+    let password = document.getElementById("password").value;
+    let users = JSON.parse(localStorage.getItem("users"));
+    let atpos = email.indexOf("@");
+    let dotpos = email.lastIndexOf(".");
+    if (email === null || email === "" || atpos < 1 || dotpos - atpos < 2) {
+      alert("Please provide your correct email ID!");
+      document.getElementById("email").focus();
       return;
     }
-  }
-  alert("User not found!");
-});
+    for (let i = 0; i < users.length; i++) {
+      console.log(users);
+      if (users[i].Email === email && users[i].Password === password) {
+  
+  
+    //     container.innerHTML = `<a href="home.html" id="back-button"><i class="fa-regular fa-circle-left fa-2x"></i></a><h1 class="heading-company">Systango</h1><div class="profile-container"> <h1 id="profile-heading">Profile Information <i class="fa-solid fa-user"></i></h1> <p class="profile"><b>Name:</b> ${users[i].Name}</p><p class="profile"><b>Email:</b> ${users[i].Email}</p><p class="profile"><b>Phone No.:</b> ${users[i].Phone_No}</p>
+    //       <a href="/login.html"
+    //   ><button>LogOut</button></a
+    // ></div>`;
+    localStorage.setItem("users1",JSON.stringify(users[i]));
+   
+    window.location.href="home2.html";
+        return;
+      }
+    }
+    alert("User not found!");
+  });
+}
 
 
 //home2 JS
 
+window.onload = function () {
+  let users1 = JSON.parse(localStorage.getItem("users1"));
+  for (let key in users1) {
+    document.getElementById(key).innerHTML = users1[key];
+  }
+};
+const logoutbtn = document.getElementById("logout-btn");
+logoutbtn.addEventListener("click", () => {
+  localStorage.removeItem("users1");
+  window.location.href="login.html";
+});
